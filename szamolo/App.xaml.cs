d26642a19f3078489d4ed0b6fff88421c90cf047
kormonditamas2222 <t.kormond2006@gmail.com>
2025-09-10 09:06:19 +0200

    Add project files.

---
+++ szamolo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace szamolo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
